9c4c7166604ed914b7f35dd586fc093883a4ec42
Bohdan <bohdan.romashchenko@gmail.com>
2020-12-13 23:25:43 +0200

    added second endpoint

--- server/initializer.py
+++ server/initializer.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import pickle
+import joblib
+import numpy as np
 
 data_path = './data/'
 models_path = './models/'
@@ -25,8 +26,12 @@ def load_datasets():
 
 def load_models():
     # read ML models
-    grid_load_model = pickle.load(open(models_path + 'gridload_model.pickle', 'rb'))
-    return grid_load_model
+    grid_load_predictor = joblib.load(models_path + 'grid_load_predictor.sav')
+    car_model_predictor = joblib.load(models_path + 'car_model_predictor.sav')
+    soc_predictor = joblib.load(models_path + 'soc_predictor.sav')
+    consumption_predictor = joblib.load(models_path + 'consumption_predictor.sav')
+
+    return grid_load_predictor, car_model_predictor, soc_predictor, consumption_predictor
 
 
 # an extract from colab
@@ -61,3 +66,44 @@ def prepare_gridload_dataset(grid_locations, gridload):
     dataset['base_load'] = base_loads
     dataset['max_load'] = max_loads
     return dataset
+
+
+def prepare_all_chargers_dataset(public_chargers, home_chargers, grid_locations):
+    all_charger_locations = pd.concat([home_chargers, public_chargers], axis=0).drop(["x", "y"], axis=1)
+    all_charger_locations['is_home_loc'] = all_charger_locations.cadaster.isin(home_chargers.cadaster).astype(int)
+    grid_cadasters = []
+    for _, charger in all_charger_locations.iterrows():
+        all_distances = [haversine_np(charger['longitude'], charger['latitude'], grid['longitude'], grid['latitude'])
+                         for _, grid in grid_locations.iterrows()]
+        grid_idx = np.argmin(all_distances)
+        grid_cadasters.append(grid_locations.iloc[grid_idx][['cadaster', 'longitude', 'latitude']])
+
+    all_charger_locations[['closest_grid', 'grid_longitude', 'grid_latitude']] = grid_cadasters
+
+    return all_charger_locations
+
+
+def haversine_np(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+
+
+def prepare_grid_chargers_dataset(grid_locations, all_charger_locations):
+    gc_dataset = grid_locations.copy()
+    gc_dataset.drop(['address', 'latitude', 'longitude', 'x', 'y', 'max_current'], axis=1, inplace=True)
+    chargers = []
+
+    for i, row in gc_dataset.iterrows():
+        grid_chargers = all_charger_locations[all_charger_locations['closest_grid'] == row['cadaster']]
+        # basically all pairs of charger cadaster/charge need for this grid at this time
+        out = [charger['cadaster'] for _, charger in grid_chargers.iterrows()]
+        chargers.append(out)
+
+    gc_dataset['chargers'] = chargers
+
+    return gc_dataset
